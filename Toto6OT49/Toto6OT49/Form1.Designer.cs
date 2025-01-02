d20b08eb9cf10a44e6459b65f8b173a2db7d6beb
Callaaa <148789803+Callaaa@users.noreply.github.com>
2025-01-03 00:02:04 +0200

    Add files via upload

---
+++ Toto6OT49/Toto6OT49/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Toto6OT49
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtB1 = new System.Windows.Forms.TextBox();
+            this.txtB2 = new System.Windows.Forms.TextBox();
+            this.txtB3 = new System.Windows.Forms.TextBox();
+            this.txtB4 = new System.Windows.Forms.TextBox();
+            this.txtB5 = new System.Windows.Forms.TextBox();
+            this.txtB6 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(89, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Генерирай числата";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Число 1:";
+            // 
+            // txtB1
+            // 
+            this.txtB1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB1.Location = new System.Drawing.Point(89, 65);
+            this.txtB1.Name = "txtB1";
+            this.txtB1.Size = new System.Drawing.Size(76, 27);
+            this.txtB1.TabIndex = 2;
+            // 
+            // txtB2
+            // 
+            this.txtB2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB2.Location = new System.Drawing.Point(89, 103);
+            this.txtB2.Name = "txtB2";
+            this.txtB2.Size = new System.Drawing.Size(76, 27);
+            this.txtB2.TabIndex = 3;
+            // 
+            // txtB3
+            // 
+            this.txtB3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB3.Location = new System.Drawing.Point(89, 140);
+            this.txtB3.Name = "txtB3";
+            this.txtB3.Size = new System.Drawing.Size(76, 27);
+            this.txtB3.TabIndex = 4;
+            // 
+            // txtB4
+            // 
+            this.txtB4.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB4.Location = new System.Drawing.Point(264, 65);
+            this.txtB4.Name = "txtB4";
+            this.txtB4.Size = new System.Drawing.Size(76, 27);
+            this.txtB4.TabIndex = 5;
+            // 
+            // txtB5
+            // 
+            this.txtB5.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB5.Location = new System.Drawing.Point(264, 103);
+            this.txtB5.Name = "txtB5";
+            this.txtB5.Size = new System.Drawing.Size(76, 27);
+            this.txtB5.TabIndex = 6;
+            // 
+            // txtB6
+            // 
+            this.txtB6.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB6.Location = new System.Drawing.Point(264, 140);
+            this.txtB6.Name = "txtB6";
+            this.txtB6.Size = new System.Drawing.Size(76, 27);
+            this.txtB6.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 19);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Число 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 19);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Число 3:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(181, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 19);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Число 4:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(181, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 19);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Число 5:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(181, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 19);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Число 6:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 352);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtB6);
+            this.Controls.Add(this.txtB5);
+            this.Controls.Add(this.txtB4);
+            this.Controls.Add(this.txtB3);
+            this.Controls.Add(this.txtB2);
+            this.Controls.Add(this.txtB1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Тото 6 от 49";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtB1;
+        private System.Windows.Forms.TextBox txtB2;
+        private System.Windows.Forms.TextBox txtB3;
+        private System.Windows.Forms.TextBox txtB4;
+        private System.Windows.Forms.TextBox txtB5;
+        private System.Windows.Forms.TextBox txtB6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Toto6OT49/Toto6OT49/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Toto6OT49
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int[] numbers = new int[6];
+            int count = 0;
+            while (count < 6)
+            {
+                int num = rand.Next(1, 50);
+                bool doesExist = false;
+                for (int i = 0; i < count; i++)
+                {
+                    if (numbers[i] == num)
+                    {
+                        doesExist = true;
+                        break;
+                    }                   
+                }
+                if(!doesExist)
+                {
+                    numbers[count] = num;
+                    count++;
+                }               
+                txtB1.Text = numbers[0].ToString();
+                txtB2.Text = numbers[1].ToString();
+                txtB3.Text = numbers[2].ToString();
+                txtB4.Text = numbers[3].ToString();
+                txtB5.Text = numbers[4].ToString();
+                txtB6.Text = numbers[5].ToString();               
+            }
+        }
+    }
+}
